9862251aab535de56914ca3c506b3b033941da7a
dianamdvdnk1 <dianamdvdnk@gmail.com>
2022-11-02 18:04:14 +0300

    Homework

--- 1909/datefunc.py
+++ 1909/datefunc.py
@@ -5,8 +5,7 @@ fordel = 400
 
 
 def date(thedate):
-    """
-    Function which testing existence of date.
+    """Function which testing existence of date.
 
     Args:
         thedate: str - variable which rebuild into day, month, year.
@@ -24,10 +23,10 @@ def date(thedate):
         return False
     # Проверка месяца
     if day <= 0 or month <= 0 or year < 0:
-        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if year % fordel == 0 and year % 100 == 0 or year % 4 == 0 and year % 100 != 0: # Проверка высокосного года
+        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]# Проверка высокосного года
+        if year % fordel == 0 and year % 100 == 0 or year % 4 == 0 and year % 100 != 0:
             months[1] = 29 # Замена дня в высокосный день
-        if day <= months[month - 1]: #Список с количеством дней, под индексом(номером месяца -1)
+        if day <= months[month - 1]:# Список с количеством дней, под индексом(номером месяца -1)
             if month <= 12: 
                 return True
     return False
